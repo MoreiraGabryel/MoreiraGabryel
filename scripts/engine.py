e7d34bcd1a26da846013bc1faba40804280919af
MoreiraGabryel <marcosgabryel25@gmail.com>
2026-01-20 17:29:22 -0300

    Update engine.py

--- scripts/engine.py
+++ scripts/engine.py
@@ -3,23 +3,34 @@ import re
 
 def main():
     issue_title = os.getenv("ISSUE_TITLE", "")
-    if not issue_title.startswith("tttp:"):
-        return
+    if not issue_title.startswith("tttp:"): return
 
-    move = issue_title.split(":")[1].split(",")
-    row_u, col_u = int(move[0]), int(move[1])
+    try:
+        coords = issue_title.split(":")[1].split(",")
+        row, col = int(coords[0]), int(coords[1])
+    except: return
 
     with open("README.md", "r", encoding="utf-8") as f:
         content = f.read()
 
+    user_move_link = rf"\[ \]\(https:\/\/github\.com\/MoreiraGabryel\/MoreiraGabryel\/issues\/new\?title=tttp:{row},{col}\)"
     
-    user_pattern = rf"\[ \]\(https:\/\/github\.com\/MoreiraGabryel\/MoreiraGabryel\/issues\/new\?title=tttp:{row_u},{col_u}\)"
-    content = re.sub(user_pattern, "❌", content)
+    if re.search(user_move_link, content):
+        content = re.sub(user_move_link, "❌", content)
+        
+        ia_patterns = [r"tttp:1,1", r"tttp:0,0", r"tttp:0,2", r"tttp:2,0", r"tttp:2,2"]
+        for p in ia_patterns:
+            ia_move_link = rf"\[ \]\(https:\/\/github\.com\/MoreiraGabryel\/MoreiraGabryel\/issues\/new\?title={p}\)"
+            if re.search(ia_move_link, content):
+                content = re.sub(ia_move_link, "⭕", content)
+                break
 
-    status_msg = "**Status:** Fim de jogo.\n> **Mensagem do Sistema:** `EU SEMPRE VENÇO`"
-    
-    # 3. Atualizar o Status no README
-    content = re.sub(r"\*\*Status:\*\*.*", status_msg, content)
+    if "[ ]" not in content:
+        status_update = "**Status:** Fim de jogo.\n> **Mensagem do Sistema:** `EU SEMPRE VENÇO`"
+    else:
+        status_update = "**Status:** Sua vez!\n> **Mensagem do Sistema:** *Aguardando sua derrota...*"
+
+    content = re.sub(r"\*\*Status:\*\*.*", status_update, content, flags=re.DOTALL)
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.write(content)
